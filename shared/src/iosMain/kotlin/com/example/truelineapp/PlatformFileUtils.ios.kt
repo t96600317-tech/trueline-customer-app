14aca71cf104709148e8cdee399843aa34c84dd8
CosmicSparX <kpranay661@gmail.com>
2026-08-30 22:31:29 +0530

    Use portable iOS clock utilities

--- shared/src/iosMain/kotlin/com/example/truelineapp/PlatformFileUtils.ios.kt
+++ shared/src/iosMain/kotlin/com/example/truelineapp/PlatformFileUtils.ios.kt
@@ -1,10 +1,12 @@
 package com.example.truelineapp
 
-import platform.Foundation.NSDate
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSFileManager
+import kotlin.time.Clock
 
-actual fun currentPlatformTimeMillis(): Long = (NSDate().timeIntervalSince1970 * 1_000).toLong()
+actual fun currentPlatformTimeMillis(): Long = Clock.System.now().toEpochMilliseconds()
 
+@OptIn(ExperimentalForeignApi::class)
 actual fun deleteLocalFile(path: String) {
     NSFileManager.defaultManager.removeItemAtPath(path, error = null)
 }
